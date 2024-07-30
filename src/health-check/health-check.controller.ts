f49679add20318797c927f9500431227ca4b54b7
Giusseppe <giusseppejpm@gmail.com>
2024-07-30 10:27:52 -0400

    message is up

--- src/health-check/health-check.controller.ts
+++ src/health-check/health-check.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HealthCheckController {
   @Get()
   healthCheck() {
-    return 'Client gateway esta corriendo';
+    return 'Client gateway is UP';
   }
 }
